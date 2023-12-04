243172448962aabf08fbccd3cbc04043b88aead7
Riccardo-Laurenti <riccardo.laurenti95@gmail.com>
2023-12-04 16:32:02 +0100

    i rewrite exercise with all bonus

--- js/script.js
+++ js/script.js
@@ -1,92 +1,102 @@
+// ! ALGORITMO BIGLIETTO TRENO FORM INPUT - Made by Riccardo Laurenti - Web Developer Full Stack
+
+// Prendo elementi dom per la logica del programma
+
+const nameSurnUser = document.getElementById('name-surname');
+
+const kmUserTravel = document.getElementById('km-travel');
+
+const scountSelectAge = document.getElementById('age-scount');
+
+const buttonGenerate = document.getElementById('generate-btn');
+
+const btnDelete = document.getElementById('btn-delete');
 
 
+// Elementi Dom da stampare 
 
-// ESERCIZIO 2 JS BIGLIETTO -TRENO -FORM :
+const showTicket = document.getElementById('show-ticket');
+
+const ticketContain = document.getElementById('ticket-container')
+
+const namePrint = document.getElementById('name-print')
+
+const carrozzaRandom = document.getElementById('carrozza-random')
+
+const cpRandom = document.getElementById('cp-random')
+
+const rate = document.getElementById('rate')
+
+const result = document.getElementById('price-ticket')
 
 
-// Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+// Al click del bottone ..
 
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio.
+buttonGenerate.addEventListener("click", function () {
 
-// Il prezzo del biglietto è definito in base ai km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+    const domTicket = showTicket;
 
+    const name = nameSurnUser.value.trim();
 
-// evento al click del pulsante Genera
+    const km = parseInt(kmUserTravel.value);
 
+    const ageSelect = scountSelectAge.value;
 
-var btnGenerate = document.getElementById("generate");
-btnGenerate.addEventListener("click",
-    
-    function () {
-        var passengerName = document.getElementById("name").value;
-        console.log("Generazione nome del passeggero:", passengerName);
+    const randCarrozza = Math.floor(Math.random() * 2900) + 1;
 
-        var km = document.getElementById("km").value;
-        console.log("Generazione dei km da percorrere:", km);
+    const cpRand = Math.floor(Math.random() * 600) + 1;
 
-        var age = document.getElementById("age").value;
-        console.log("Generazione dell'età:", age);
+    let priceCalc = km * 0.21;
 
-        var ticketPrice = Math.floor(km * 0.21);
-        var discount;
-        var discountType;
-        console.log("Prezzo del biglietto pieno:", ticketPrice);
+    let priceBase = priceCalc;
 
-        // calcolo sconto per età
-        if (age == "minorenne") {
-            discount = ticketPrice * 0.2; 
-            discountType = "Sconto Bronze";
-        } else if (age =="over65") {
-            discount = ticketPrice * 0.4;
-            discountType = "Sconto Silver";
-        } else {
-            discount = 0;
-            discountType = "Biglietto standard";
+    let rateTicket = 'Tariffa base';
+
+    // Validazione
+    if (!name || isNaN(km) || km < 1) {
+        alert("Inserisci i valori corretti !")
+
+    } else {
+
+        if (ageSelect === 'min') {
+            priceBase = priceCalc * 0.8;
+
+            rateTicket = 'Sconto 20%';
+
+        } else if (ageSelect === 'over') {
+            priceBase = priceCalc * 0.6;
+
+            rateTicket = 'Sconto 40%';
         }
 
-        // calcolo prezzo finale
-        var finalPrice = ticketPrice - discount;
-        console.log("Prezzo dello sconto:", discount);
-        console.log("Prezzo del biglietto finale:", finalPrice);
-        console.log("Tipo di sconto:", discountType);
-
-        // creazione dati biglietto in pagina
-        document.getElementById("passenger").innerHTML = passengerName;
-        document.getElementById("discount").innerHTML = discountType;
-        document.getElementById("ticket-price").innerHTML = finalPrice + "€";
-
-        var wagonNumber = Math.floor(Math.random() * 9) + 1;
-        console.log("Creazione numero di carrozza:", wagonNumber);
-        document.getElementById("wagon").innerHTML = wagonNumber;
-
-        var cpCode = Math.floor(Math.random() * 10000) + 90000;
-        console.log("Creazione numero CP:", cpCode);
-        document.getElementById("cp-code").innerHTML = cpCode;
-        
-        // apparizione sezione ticket
-        document.getElementById("ticket-section").className = "bottom show";
-    }   
- 
-)
-
-// evento al click del pulsante Annulla
-
-var btnGenerate = document.getElementById("cancel");
-btnGenerate.addEventListener("click",
-    function () {
-        // sparizione parametri ticket
-        document.getElementById("name").value = "";
-        document.getElementById("km").value = "";
-        document.getElementById("age").value = "";
-
-        document.getElementById("passenger").innerHTML = "";
-        document.getElementById("discount").innerHTML = "";
-        document.getElementById("ticket-price").innerHTML = "";
-        document.getElementById("wagon").innerHTML = "";
-        document.getElementById("cp-code").innerHTML = "";
-
-        // sparizione sezione ticket
-        document.getElementById("ticket-section").className = "bottom hidden";
+        ticketContain.classList.remove('d-none')
     }
 
-)
+    domTicket.innerHTML;
+
+    namePrint.innerHTML = name;
+
+    rate.innerHTML = rateTicket;
+
+    carrozzaRandom.innerHTML = randCarrozza;
+
+    cpRandom.innerHTML = cpRand;
+
+    result.innerHTML = priceBase.toFixed(2) + ' €';
+
+
+})
+
+
+// Al click cancello ..
+
+btnDelete.addEventListener("click", function () {
+
+    nameSurnUser.value = '';
+
+    kmUserTravel.value = '';
+
+    scountSelectAge.value = '';
+
+    ticketContain.classList.add('d-none')
+})
